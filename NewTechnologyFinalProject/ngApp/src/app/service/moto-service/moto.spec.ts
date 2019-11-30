615a8a16666096f05a38b1f5a64358237480776d
hunghuynh9822 <31771988+hunghuynh9822@users.noreply.github.com>
2019-11-30 23:12:56 +0700

    clean code

--- NewTechnologyFinalProject/ngApp/src/app/service/moto-service/moto.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Moto } from './moto';
-
-describe('Moto', () => {
-  it('should create an instance', () => {
-    expect(new Moto()).toBeTruthy();
-  });
-});
--- NewTechnologyFinalProject/ngApp/src/app/service/moto-service/moto.ts
+++
@@ -1,9 +0,0 @@
-export class Moto {
-    _id: string;
-    name : String;
-    cost: String;
-    idKind: String;
-    idGear:String;
-    count:String;
-    image:String;
-}
